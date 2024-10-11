878e78309f083a4622ab83589e03c39d7ffd52c7
Mohammed faisal S <108690043+mdfaisal001@users.noreply.github.com>
2024-10-11 17:50:23 +0530

    Add files via upload

---
+++ 100DaysOfDSA/Day6/intersectionOf2Array.cpp
@@ -0,0 +1,87 @@
+class Solution
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        unordered_set<int> set1(begin(nums1), end(nums1));
+        unordered_set<int> set2;
+        for (int &nums : nums2)
+        {
+            if (set1.find(nums) != set1.end())
+            {
+                set2.insert(nums);
+            }
+        }
+        vector<int> newVector;
+        for (auto &it : set2)
+        {
+            newVector.push_back(it);
+        }
+
+        return newVector; // O(n)
+    }
+}; // more optimized
+
+class Solution
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size())
+        {
+            if (nums1[i] == nums2[j])
+            {
+                if (result.empty() || result.back() != nums1[i])
+                {
+                    result.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+        return result;
+    }
+}; // optimized one O(nlogn)
+
+// brute force approach
+class Solution
+{
+public:
+    vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+    {
+        set<int> toStore;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        for (int i = 0; i < n1; i++)
+        {
+            for (int j = 0; j < n2; j++)
+            {
+                if (nums1[i] == nums2[j])
+                {
+                    toStore.insert(nums1[i]);
+                }
+            }
+        }
+
+        vector<int> newArray(toStore.begin(), toStore.end());
+        return newArray;
+    }
+};
